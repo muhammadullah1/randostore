803b90b07957c2423edcbe7fc79f5fd036cb5ca2
muhammadullah1 <muhammadullah403@gmail.com>
2022-12-21 18:10:00 +0500

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,16 +9,16 @@ const Navbarr = () => {
   const itemsList = useItems();
   const { cart } = itemsList;
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" className='navbar-main'>
       <Container>
         <Navbar.Brand><Link to="allitems">Rando Store</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link><Link to="allitems">All Items</Link></Nav.Link>
-            <Nav.Link><Link to="checkeditems">Checked Items</Link></Nav.Link>
-            <Nav.Link><Link to="additem">Add item</Link></Nav.Link>
-            <Nav.Link className='btn btn-outline-secondary px-3 mx-5'><Link to="checkeditems">Cart <Badge bg="text-white">{cart.length}</Badge></Link></Nav.Link>
+            <Nav.Link><Link to="allitems">AllItems</Link></Nav.Link>
+            <Nav.Link><Link to="checkeditems">CheckedItems</Link></Nav.Link>
+            <Nav.Link><Link to="additem">AddItem</Link></Nav.Link>
+            <Nav.Link className='btn btn-outline-secondary px-3 mx-5 rounded-0'><Link to="checkeditems">Cart <Badge bg="text-white">{cart.length}</Badge></Link></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/pages/Additems.jsx
+++ src/pages/Additems.jsx
@@ -4,12 +4,14 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Alert from 'react-bootstrap/Alert';
 import { useItems } from '../Hooks/FetchItemsContext';
 
 const Additems = () => {
   const itemsList = useItems();
   const { addItem } = itemsList;
   const [formData, setFormData] = useState({});
+  const [show, setShow] = useState(true);
 
   const handleChange = event => {
     const { name, value } = event.target;
@@ -18,7 +20,10 @@ const Additems = () => {
   const handleSubmit = event => {
     event.preventDefault();
     addItem(formData);
+    setFormData({})
+    event.target.reset();
   };
+
   return (
     <Container className='container mx-auto my-5' fluid="md">
       <h1 className='text-center my-3'>Add Items</h1>
@@ -26,24 +31,16 @@ const Additems = () => {
       <Col xs={10} md={8} lg={6} className="mx-auto">
       <Form onSubmit={handleSubmit}>
       <Form.Group className="my-5" controlId="formBasicEmail">
-        {/* <Form.Label>Name</Form.Label> */}
-        <Form.Control type="text" name="name" placeholder="Enter Item Name" className='rounded-0' onChange={handleChange}/>
+        <Form.Control required type="text" name="name" placeholder="Enter Item Name" className='rounded-0 p-2 input-field' onChange={handleChange}/>
       </Form.Group>
-
       <Form.Group className="mb-5" controlId="formBasicPassword">
-        {/* <Form.Label>Price</Form.Label> */}
-        <Form.Control type="text" name="price" placeholder="Price" className='rounded-0' onChange={handleChange}/>
+        <Form.Control required type="text" name="price" placeholder="Price" className='rounded-0 p-2 input-field' onChange={handleChange}/>
       </Form.Group>
-
       <Form.Group className="mb-5" controlId="formBasicPassword">
-        {/* <Form.Label>Image</Form.Label> */}
-        <Form.Control type="text" name="img" placeholder="Image" className='rounded-0' onChange={handleChange}/>
+        <Form.Control required type="text" name="img" placeholder="Image" className='rounded-0 p-2 input-field' onChange={handleChange}/>
       </Form.Group>
-
-
-
       <div className="d-grid gap-2 my-5">
-      <Button variant="primary"  type="submit" size="lg" className='mx-auto rounded-0'>
+      <Button type="submit" size="lg" className='mx-auto rounded-0 item-add-btn'>
         Add Item
       </Button>
     </div>
